54616db9575dd35c603112c9be2afa82c77c7b12
Tao Wen <taowen@gmail.com>
2023-03-19 21:15:59 +0800

    new api

--- src/sample/api.ts
+++ src/sample/api.ts
@@ -1,2 +1,10 @@
 export function executeOldCommand(commandClass: any) {
 }
+
+export class ICommand {}
+
+export function registerNewCommand(commandClass: new () => ICommand) {
+}
+
+export function executeNewCommand(commandClass: new () => ICommand) {
+}
