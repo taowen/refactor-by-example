a52b8aef214e48db0c9dc4d89935753debd636fb
Tao Wen <taowen@gmail.com>
2023-03-21 23:10:51 +0800

    extract key symbol from current selection

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as childProcess from 'child_process'
 import { parsePatch } from './parsePatch';
 import * as fs from 'fs'
 import * as path from 'path'
-
+import * as https from 'https'
 
 function getWorkspaceDir() {
 	if (!vscode.workspace.workspaceFolders) {
@@ -55,14 +55,50 @@ const execShell = (cmd: string, options: childProcess.ExecOptions) =>
         });
     });
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
 
+async function openaiComplete(content: string) {
 	const conf = vscode.workspace.getConfiguration('refactorByExample');
 	const apiUrl = conf.get<string>('apiUrl');
 	const apiKey = conf.get<string>('apiKey');
 	console.log({ apiUrl, apiKey })
+	return new Promise<string>((resolve, reject) => {
+		const req = https.request(`${apiUrl}/v1/chat/completions`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${apiKey}`,
+				'Content-Type': 'application/json'
+			},
+		}, (res) => {
+			if (res.statusCode !== 200) {
+				reject(new Error(`unexpected response statusCode: ${res.statusCode}`))
+			}
+			res.setEncoding('utf8');
+			const chunks: string[] = [];
+			res.on('data', function (chunk) {
+				chunks.push(chunk);
+			});
+			res.on('end', function() {
+				const respText = chunks.join();
+				console.log(respText);
+				resolve(JSON.parse(respText).choices[0].message.content);
+			})
+		})
+		const reqObj = {
+			"model": "gpt-3.5-turbo",
+			"messages": [
+				{
+					"role": "user",
+					content,
+				}
+			]
+		};
+		req.write(JSON.stringify(reqObj));
+		req.end();
+	})
+}
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -72,15 +108,46 @@ export function activate(context: vscode.ExtensionContext) {
 		if (!workspaceDir) {
 			return;
 		}
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		const selectionText = editor.document.getText(editor.selection)
 		const examples = await getExamples();
 		const example = examples[exampleId]
 		if (!example?.commit) {
 			console.log('missing commit from example')
 			return;
 		}
+		if (!example?.symbol) {
+			console.log('missing symbol from example')
+			return;
+		}
 		const patchContent = await execShell(`git show ${example.commit}`, { cwd: workspaceDir })
 		const blocks = parsePatch(patchContent);
-		console.log(blocks)
+		const promptLines = ['extract symbol from code snippets:', '===code snippets==='];
+		for(const block of blocks) {
+			if (block.oldContent) {
+				promptLines.push('```')
+				promptLines.push(block.oldContent);
+				promptLines.push('```')
+			}
+		}
+		promptLines.push('===symbol===')
+		promptLines.push(example.symbol)
+		promptLines.push('===code snippets===')
+		promptLines.push('````')
+		promptLines.push(selectionText)
+		promptLines.push('````')
+		promptLines.push('===symbol===')
+		const toRefactorSymbol = await openaiComplete(promptLines.join('\n'))
+		vscode.window
+			.showInformationMessage(`Do you want to refactor ${toRefactorSymbol}`, "Yes", "No")
+			.then(answer => {
+				if (answer === "Yes") {
+				// Run function
+				}
+			})
 	});
 
 	vscode.window.createTreeView('refactoringExamples', {
